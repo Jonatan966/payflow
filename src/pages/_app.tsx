9590a36bb4002da2f1177061f675890a053d9c7d
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-07-01 17:57:37 -0300

    feat: activate auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppThemeProvider } from '../contexts/app-theme-provider'
+import { AuthProvider } from '../contexts/auth-provider'
 import { GlobalStyles } from '../styles/global'
 
 export default function MyApp ({ Component, pageProps }: any) {
   return (
     <AppThemeProvider>
-      <GlobalStyles/>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <GlobalStyles/>
+        <Component {...pageProps} />
+      </AuthProvider>
     </AppThemeProvider>
   )
 }
