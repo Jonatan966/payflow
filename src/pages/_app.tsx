063dbcc3fb0c93240ebfed6f4ff7d700c386f4fa
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-07-01 19:30:05 -0300

    feat: config react-hot-toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { Toaster } from 'react-hot-toast'
+
 import { AppThemeProvider } from '../contexts/app-theme-provider'
 import { AuthProvider } from '../contexts/auth-provider'
 import { GlobalStyles } from '../styles/global'
@@ -6,6 +8,7 @@ export default function MyApp ({ Component, pageProps }: any) {
   return (
     <AppThemeProvider>
       <AuthProvider>
+        <Toaster />
         <GlobalStyles/>
         <Component {...pageProps} />
       </AuthProvider>
