b11980a9713221bf1ccd4f1e17f158693cd9ecd9
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-07-01 17:59:42 -0300

    feat: show user information in header

--- src/components/user-profile-header.tsx
+++ src/components/user-profile-header.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { ReactNode } from 'react'
 
+import { useAuth } from '../hooks/use-auth'
+
 import { UserProfileHeaderContainer } from '../styles/components/user-profile-header'
 
 interface UserProfileHeaderProps {
@@ -8,15 +10,17 @@ interface UserProfileHeaderProps {
 }
 
 export function UserProfileHeader ({ children }: UserProfileHeaderProps) {
+  const { user } = useAuth()
+
   return (
     <UserProfileHeaderContainer className={children ? 'has-footer' : ''}>
       <div className="profile">
         <div className='main-info'>
-          <span>Olá, <strong>Jonas</strong></span>
+          <span>Olá, <strong>{user?.name}</strong></span>
           <p>Mantenha suas contas em dia</p>
         </div>
 
-        <Image width={48} height={48} src='/images/profile.jpg' className='profile-image' />
+        <Image width={48} height={48} src={user?.image ?? '/images/profile.jpg'} className='profile-image' />
       </div>
 
       <div className="header-footer">
