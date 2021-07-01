56bbf116144f004c8f80054b4ef2c107664163a5
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-07-01 17:58:50 -0300

    feat: implement sign in function into button

--- src/components/sign-in-with-google-button.tsx
+++ src/components/sign-in-with-google-button.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 
 import { SignInWithGoogleButtonContainer } from '../styles/components/sign-in-with-google-button'
+import { useAuth } from '../hooks/use-auth'
 
 export function SignInWithGoogleButton () {
+  const { handleSignIn } = useAuth()
+
   return (
-    <SignInWithGoogleButtonContainer>
+    <SignInWithGoogleButtonContainer onClick={handleSignIn}>
       <Image src='/images/google.png' width={24} height={24} />
       <strong></strong>
       <span>Entrar com Google</span>
