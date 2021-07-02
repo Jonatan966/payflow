60a532f598945e422412ba7ad3a6501685c15761
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-07-02 00:29:03 -0300

    feat: spinner custom style in actions btn footer

--- src/styles/components/action-buttons-footer.ts
+++ src/styles/components/action-buttons-footer.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { SpinnerContainer } from './spinner'
 
 export const ActionButtonFooterContainer = styled.footer`
   display: grid;
@@ -15,5 +16,11 @@ export const ActionButtonFooterContainer = styled.footer`
     &.main {
       color: ${ctx => ctx.theme.colors.primary};
     }
+  }
+
+  ${SpinnerContainer} {
+    margin: 0.75rem auto;
+    grid-column-start: 1;
+    grid-column-end: 3;
   }
 `
