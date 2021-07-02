f24e8565929f65bd15a8db797c345821af9e4d55
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-07-02 18:56:04 -0300

    feat: logic on bill form

--- src/pages/add-bill.tsx
+++ src/pages/add-bill.tsx
@@ -1,41 +1,123 @@
+import Router from 'next/router'
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
 import { FaArrowLeft, FaBarcode } from 'react-icons/fa'
 import { FiFileText, FiXCircle } from 'react-icons/fi'
 import { RiWalletLine } from 'react-icons/ri'
 
 import { TextInput } from '../components/text-input'
+import { Bill } from '../interfaces/bill'
+import { api } from '../services/api'
 import { ActionButtonFooterContainer } from '../styles/components/action-buttons-footer'
+import { SpinnerContainer } from '../styles/components/spinner'
 import { AddBillPageContainer } from '../styles/pages/add-bill-page'
+import { Boleto } from '@mrmgomes/boleto-utils'
 
 export default function AddBillPage () {
+  const [isSavingBill, setIsSavingBill] = useState(false)
+  const { register, handleSubmit, setValue } = useForm()
+
+  async function handleAddBill ({ name, amount, barcode, dueDate }: Bill) {
+    const addBill = api.post('/add-bill', {
+      name,
+      amount: Number(amount),
+      barcode,
+      dueDate
+    })
+
+    setIsSavingBill(true)
+
+    const insertResult = await toast.promise(addBill, {
+      error: 'Não foi possível salvar este boleto',
+      loading: 'Salvando boleto. . .',
+      success: 'Boleto salvo com sucesso!'
+    })
+
+    if (insertResult.status === 201) {
+      await Router.replace('/')
+    }
+
+    setIsSavingBill(false)
+  }
+
+  useEffect(() => {
+    if (!Router.query['before-scan']) {
+      return
+    }
+
+    const storagedBillData = sessionStorage.getItem('@payflow:scanned-bill')
+
+    if (!storagedBillData) {
+      return
+    }
+
+    const parsedBillData = JSON.parse(storagedBillData) as Boleto
+
+    setValue('teste', 'bom dia')
+    setValue('dueDate', parsedBillData.vencimento.split('T')[0])
+    setValue('amount', parsedBillData.valor)
+    setValue('barcode', parsedBillData.linhaDigitavel)
+
+    sessionStorage.removeItem('@payflow:scanned-bill')
+  }, [])
+
   return (
     <AddBillPageContainer>
       <header>
-        <FaArrowLeft size={20} />
+        <FaArrowLeft size={20} onClick={() => Router.back()} />
         <h2>Preencha os dados do boleto</h2>
       </header>
 
-      <form>
+      <form onSubmit={handleSubmit(handleAddBill)}>
         <TextInput
           icon={<FiFileText size={25} />}
           placeholder='Nome do boleto'
+          required
+          {...register('name')}
         />
         <TextInput
           icon={<FiXCircle size={25} />}
           placeholder='Vencimento'
+          type='date'
+          required
+          {...register('dueDate')}
         />
         <TextInput
           icon={<RiWalletLine size={25} />}
           placeholder='Valor'
           type='number'
+          step='any'
+          required
+          {...register('amount')}
         />
         <TextInput
           icon={<FaBarcode size={25} />}
           placeholder='Código'
+          required
+          {...register('barcode')}
         />
 
         <ActionButtonFooterContainer>
-          <button>Cancelar</button>
-          <button className='main'>Cadastrar</button>
+          {isSavingBill
+            ? <SpinnerContainer size={25} />
+            : (
+              <>
+                <button
+                  type='button'
+                  onClick={() => Router.replace('/')}
+                >
+                  Cancelar
+                </button>
+                <button
+                  type='submit'
+                  className='main'
+                >
+                  Cadastrar
+                </button>
+              </>
+              )
+          }
         </ActionButtonFooterContainer>
       </form>
     </AddBillPageContainer>
